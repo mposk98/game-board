df27d7a625940c2dae6dfb683130a63212b60042
mposk98 <anythingand42@gmail.com>
2020-04-18 22:01:55 +0300

    add less

--- src/game-board/index.js
+++ src/game-board/index.js
@@ -2,7 +2,7 @@ import compose from '../utils/compose';
 import createElements from '../utils/create-elements';
 import makeTree from './make-tree';
 import addLogic from './logic';
-import './styles.css';
+import './styles.less';
 
 const GameBoard = (options) => compose(addLogic, createElements, makeTree)(options);
 
